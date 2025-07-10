581fdb3c5c8447da28a369bce51125073403fbe6
Timothy <timothysuldayan1@gmail.com>
2025-07-09 21:39:58 -0700

    initial commit

---
+++ punch-meter-backend/src/main/java/com/example/punch_meter_backend/PunchMeterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.punch_meter_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchMeterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchMeterBackendApplication.class, args);
+	}
+
+}
---
+++ punch-meter-backend/src/test/java/com/example/punch_meter_backend/PunchMeterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.punch_meter_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchMeterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
